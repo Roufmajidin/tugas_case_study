8a30d9f37855de53e32618ba72d2a0db76e5264d
Roufmajidin <roufmajidin777@gmail.com>
2024-09-30 05:26:12 +0700

    feat: refactor widget

---
+++ lib/shared_widget/gudang_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class GudangWidget extends StatelessWidget {
+  String subtitile;
+  
+  String title;
+
+   GudangWidget({
+    super.key,
+    required this.title,
+    required this.subtitile,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.grey.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(8), // Add border
+      ),
+      child: Row(
+        children: [
+          Container(
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.4),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Icon(
+                Icons.label,
+                color: Colors.purple,
+              )),
+           Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(subtitile),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/shared_widget/profile_widget.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/material.dart';
+
+class ProfileWidget extends StatelessWidget {
+  const ProfileWidget({
+    super.key,
+    required this.nama,
+    required this.jabatan,
+    required this.kode_gudang,
+  });
+  final String nama;
+  final bool jabatan;
+  final String kode_gudang;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: Colors.grey.withOpacity(0.4),
+            borderRadius: BorderRadius.circular(50), // Add border
+          ),
+          child: const ClipRect(
+            child: Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Welcome Back, $nama",
+                style: const TextStyle(fontWeight: FontWeight.w700),
+              ),
+              Text("Admin Gudang : $kode_gudang"),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
